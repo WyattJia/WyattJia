476f0da5465e3e1c7616f1db3657f936896d3df7
贾伟川 <wellschuan@gmail.com>
2018-04-15 20:21:05 +0800

    Update readme, mv description to python file

--- consistent_hashing/consistent-hashing.py
+++ consistent_hashing/consistent-hashing.py
@@ -2,6 +2,12 @@
 
 """
 Consistent Hashing demo in Python.
+~ 哈希一致性 Python 语言实现
+
+   ~ 关键抽象
+     * Entry 放入 cache 服务器中的对象
+     * Server 真正放入缓存对象的 cache 服务器
+     * Cluster 服务器集群。（假设会维护一组服务器，这相当于是一组服务器的代理，接受  put, get 请求，通过一定算法（普通取余或者哈希一致性将请求转发给特定的 server ）
 """
 
 from hashlib import md5
