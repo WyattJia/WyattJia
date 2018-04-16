9188a412dae76657b98658421d4b6343ac5a888b
贾伟川 <wellschuan@gmail.com>
2018-04-16 18:26:11 +0800

    Add not consistent hashing

--- consistent_hashing/not-consistent-hashing.py
+++ consistent_hashing/not-consistent-hashing.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python3
+# /usr/bin/env python3
 
 """
 Consistent Hashing demo in Python.
@@ -7,17 +7,20 @@ Consistent Hashing demo in Python.
    ~ 关键抽象
      * Entry 放入 cache 服务器中的对象
      * Server 真正放入缓存对象的 cache 服务器
-     * Cluster 服务器集群。（假设会维护一组服务器，这相当于是一组服务器的代理，接受  put, get 请求，通过一定算法（普通取余或者哈希一致性将请求转发给特定的 server ）
+     * Cluster 服务器集群。（假设会维护一组服务器，这相当于是一组服务器的代理，
+       接受  put, get 请求，通过一定算法（普通取余或者哈希一致性将请求转发给特定的 server ）
 """
 
 from hashlib import md5
 from typing import Dict, List
 
-class Entry:
+
+class Entry(str):
 
     _key = ''
 
     def __init__(self, key):
+        super().__init__()
         self._key = key
 
     def __str__(self):
@@ -31,14 +34,13 @@ class Server:
 
     def __init__(self, name):
         self._name = name
-        # _entries key and value should be Entry type.
-        self._entries = {}  # type: Dict[Entry, Entry]
+        self._entries: Dict[Entry, Entry] = {}
 
-    def put(self, e:Entry) -> None:
+    def put(self, e: Entry) -> None:
         self._entries[e] = e
 
-    def get(self, e:Entry) -> dict:
-        return self._entries[e]
+    def get(self, e: Entry) -> dict:
+        return self._entries.get(e)
 
 
 class Cluster:
@@ -48,33 +50,25 @@ class Cluster:
     _servers: List[Server] = []
     _size = 0
 
-    @staticmethod
-    def string_hashcode(s: str) -> str:
-        h = 0
-        for c in s:
-            h = (31 * h + ord(c)) & 0xFFFFFFFF
-        return ((h + 0x80000000) & 0xFFFFFFFF) - 0x80000000
+    def put(self, e: Entry) -> None:
+        _index = Cluster.string_hashcode(e) % self._size
+        self._servers[_index].put(e)
 
-    def put(self, e:Entry) -> None:
-        _index = string_hashcode(e) % size
-        for i in _servers:
-            # typeof(i) == Server
-            if i._name == _index:
-                i.put(e)
+    @staticmethod
+    def string_hashcode(key, encoding='utf-8'):
+        m = md5(key.encode(encoding)).hexdigest()
+        return int(m, 16)
 
     def get(self, e: Entry) -> Entry:
-        _index = string_hashcode(e) % size
-        for i in _servers:
-            # typeof(i) == Server
-            if i._name == _index:
-                return i.get(e)
+        _index = Cluster.string_hashcode(e) % self._size
+        return self._servers[_index].get(e)
 
-    def add_server(self, s:Server) -> bool:
-        if _size >= _SERVER_SIZE_MAX or len(_servers) >= _SERVER_SIZE_MAX:
+    def add_server(self, s: Server) -> bool:
+        if self._size >= self._SERVER_SIZE_MAX or len(self._servers) >= self._SERVER_SIZE_MAX:
             return False
-        elif len(_servers) < _SERVER_SIZE_MAX:
-            _servers.append(server)
-            self._size = _size + 1
+        elif len(self._servers) < self._SERVER_SIZE_MAX:
+            self._servers.append(s)
+            self._size = self._size + 1
             return True
 
 
@@ -89,15 +83,16 @@ def create_cluster():
     return c
 
 
-def find_entries(c: Cluster, entries: List) -> str:
+def find_entries(c: Cluster, entries: List) -> None:
     for e in entries:
         if e == c.get(e):
-            return '重新找到了 entry: {}'.format(str(e))
+            print("重新找到了 entry: {}".format(str(e)))
         else:
-            return "entry {} 已失效".format(str(e))
+            print("entry {} 已失效".format(str(e)))
 
 
 def main():
+
     c = create_cluster()
     entries = []
     entry_items = ["i", "have", "a", "pen", "an", "apple",  "applepen",
@@ -116,4 +111,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
