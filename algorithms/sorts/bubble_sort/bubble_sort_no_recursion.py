fb6f43cdf70359ab2ecc52b9157af4ddf817840d
贾伟川 <wellschuan@gmail.com>
2018-10-03 01:13:50 +0800

    Update no recursion bubble sort

---
+++ algorithms/sorts/bubble_sort/bubble_sort_no_recursion.py
@@ -0,0 +1,14 @@
+# bubble_sort
+
+def bubble_sort(nums):
+    counts = len(nums)
+    for i in range(0, counts):
+        for j in range(i + 1, counts):
+            if nums[i] > nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+
+    return nums
+
+if __name__ == '__main__':
+    l = [1, 9, 7, 10, 7, 5, 199, 5]
+    print(bubble_sort(l))
