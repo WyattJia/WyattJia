9941527beb70ca4129b3b2c00659ba48a3c839a9
Wyatt Jia <i@eventloop.live>
2022-01-26 23:57:22 +0800

    Delete travis.sh

--- travis.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-echo 'Testing travis...'
-bundle exec jekyll build
-bundle exec htmlproofer ./_site --only-4xx
-
-<li class="item"> <a class="link" href="https://www.douban.com/people/bitstring">Douban</a> </li>
-<li class="item"> <a class="link" href="http://music.163.com/#/user/home?id=188483">Music</a> </li>
